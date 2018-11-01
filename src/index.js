e346c09fd30f44bbf10d8763c652607070859924
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2018-11-01 11:44:42 +0100

    added logic to have x and o on alternative clicks

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 function Square(props) {
   return (
-    <button className="square" onClick={() => props.onClick()}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -11,12 +11,16 @@ function Square(props) {
 
 class Board extends React.Component {
   state = {
-    squares: Array(9).fill(null)
+    squares: Array(9).fill(null),
+    xIsNext: true
   };
   handleClick = i => {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   };
   renderSquare(i) {
     return (
